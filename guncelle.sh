cce6883303b5709a1fd4577f3b7064004a0f313a
CaylakPenguen <caylakpenguen@gmail.com>
2014-02-06 20:23:18 +0200

    entry Prş Şub  6 20:23:18 EET 2014

--- guncelle.sh
+++ guncelle.sh
@@ -6,6 +6,8 @@
 git add .
 git commit -m "entry `date`"
 git push origin master
-
+sleep 5
+# Rss Ping Serivsleri için.
+wget -O /dev/null -q  http://pingomatic.com/ping/?title=CaylakPenguen+Nano+Blog&blogurl=http%3A%2F%2Fcaylakpenguen.github.io&rssurl=CaylakPenguen+Nano+Blog&chk_weblogscom=on&chk_blogs=on&chk_feedburner=on&chk_newsgator=on&chk_myyahoo=on&chk_pubsubcom=on&chk_blogdigger=on&chk_weblogalot=on&chk_newsisfree=on&chk_topicexchange=on&chk_google=on&chk_tailrank=on&chk_skygrid=on&chk_collecta=on&chk_superfeedr=on
 
 
